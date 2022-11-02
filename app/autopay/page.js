13aac180b10ea63fd52a65a528f1b2980d024a3c
baronrocketmail <baronrocketmail@yahoo.com>
2022-11-02 09:52:17 -0700

    initial

--- app/autopay/page.js
+++ app/autopay/page.js
@@ -1,29 +1,58 @@
-import MenuLinks from "../(components)/MenuLinks";
+"use client";
 
-async function getAuthMethods(){
-    const authMethods = await fetch("http://localhost:3000/api/authMethods")
-    return authMethods.json()
+import MenuLinks from "../(components)/MenuLinks";
+import {useState, use} from "react";
+//
+async function getAutopayStatus(){
+    const autopayStatus = fetch("http://localhost:3000/api/autopayStatus")
+    return autopayStatus.json()
 }
 
-export default async function Autopay(){
+export default function Page(){
 
-    const authMethods = await getAuthMethods()
+//    const autopayInitialStatus = use(getAutopayStatus())
 
-    let authButtons = []
-
-    for(let elem in authMethods){
-        if (authMethods[elem].indexOf("@")!=-1) authButtons.push(<button className={"card"}><p>{authMethods[elem]}</p></button>)
-        else if (authMethods[elem].indexOf("(") != -1) authButtons.push(<button className={"card"}><p>{authMethods[elem]}</p></button>)
-    }
-
-    let menuLinksObjArray = [{name: "<------", url: "/"}, {name: "autopay: ", url: "/"}]
+//    const [autopayStatus, setAutopayStatus] = useState(" ")
 
     return(
             <div>
-                <MenuLinks objArray={menuLinksObjArray}/>
-                <div className={"grid"}>
-                    {authButtons}
-                </div>
-            </div>    
+            <MenuLinks objArray = {[{name: "<------", url: "/"}, {name: "autopay: " , url: "/"}]}/>
+            </div>
     )
 }
+
+
+////"use client";
+//
+//import MenuLinks from "../(components)/MenuLinks";
+//async function getAuthMethods(){
+//    const authMethods = await fetch("http://localhost:3000/api/authMethods")
+//    return authMethods.json()
+//}
+//
+////async function sendAuthCode(email, url){
+////    await fetch("http://localhost:3000/api/sendAuthCode", { method: "POST", headers:  {"Content-Type":"application/json"},  body: JSON.stringify({email: email, url: url})})
+////}
+//
+//export default async function Autopay(){
+//
+//    const authMethods = await getAuthMethods()
+//
+//    let authButtons = []
+//
+//    for(let elem in authMethods){
+//        if (authMethods[elem].indexOf("@")!=-1) authButtons.push(<button className={"card"}><p>{authMethods[elem]}</p></button>)
+//        else if (authMethods[elem].indexOf("(") != -1) authButtons.push(<button className={"card"}><p>{authMethods[elem]}</p></button>)
+//    }
+//
+//    let menuLinksObjArray = [{name: "<------", url: "/"}, {name: "autopay: ", url: "/"}]
+//
+//    return(
+//            <div>
+//                <MenuLinks objArray={menuLinksObjArray}/>
+//                <div className={"grid"}>
+//                    {authButtons}
+//                </div>
+//            </div>
+//    )
+//}
